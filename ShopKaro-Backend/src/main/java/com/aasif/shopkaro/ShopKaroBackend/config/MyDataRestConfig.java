e7ff365bd0f1afe19155e1abdc0bfcf4f9940284
razaaasif <aasifraza9123@gmail.com>
2021-11-01 23:11:16 +0530

    created backend

---
+++ ShopKaro-Backend/src/main/java/com/aasif/shopkaro/ShopKaroBackend/config/MyDataRestConfig.java
@@ -0,0 +1,32 @@
+package com.aasif.shopkaro.ShopKaroBackend.config;
+
+import com.aasif.shopkaro.ShopKaroBackend.entity.Product;
+import com.aasif.shopkaro.ShopKaroBackend.entity.ProductCategory;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.rest.core.config.RepositoryRestConfiguration;
+import org.springframework.data.rest.webmvc.config.RepositoryRestConfigurer;
+import org.springframework.http.HttpMethod;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+@Configuration
+public class MyDataRestConfig implements RepositoryRestConfigurer {
+
+    @Override
+    public void configureRepositoryRestConfiguration(RepositoryRestConfiguration config, CorsRegistry cors) {
+        HttpMethod[] theUnsupportedActions = {HttpMethod.PUT,HttpMethod.POST,HttpMethod.DELETE};
+        //disable HTTP methods for Products PUT,POST and DELETE
+        config.getExposureConfiguration()
+                .forDomainType(Product.class)
+                .withItemExposure(((metdata, httpMethods) -> httpMethods.disable(theUnsupportedActions)))
+                .withCollectionExposure(((metdata, httpMethods) -> httpMethods.disable(theUnsupportedActions)));
+        //disbale for product category
+        config.getExposureConfiguration()
+                .forDomainType(ProductCategory.class)
+                .withItemExposure(((metdata, httpMethods) -> httpMethods.disable(theUnsupportedActions)))
+                .withCollectionExposure(((metdata, httpMethods) -> httpMethods.disable(theUnsupportedActions)));
+
+    }
+
+
+
+
+}
