edd54cbfe1cb2151b0df0ca904a9925d00a8d6c1
razaaasif <aasifraza9123@gmail.com>
2021-11-04 10:17:50 +0530

    finished adding drop-down list intocheckout form

--- ShopKaro-Frontend/src/app/components/checkout/checkout.component.ts
+++ ShopKaro-Frontend/src/app/components/checkout/checkout.component.ts
@@ -59,6 +59,11 @@ export class CheckoutComponent implements OnInit {
       })
     });
 
+    this.cartService.totalPrice.subscribe(
+      data =>{
+        this.totalPrice = data
+      }
+    )
     const startMonth :number = new Date().getMonth()+1;
     console.log("StartMonth : "+startMonth);
 
@@ -93,6 +98,8 @@ export class CheckoutComponent implements OnInit {
   onSubmit(){
     console.log("Handling the submit button....");
     console.log(this.checkoutFormGroup.get('customer').value);
+    console.log("shipping address country is : "+this.checkoutFormGroup.get('shippingAddress').value.country.name);
+    console.log("shipping address state is : "+this.checkoutFormGroup.get('shippingAddress').value.state.name);
   }
 
   copyShippingAddressToBillingAddress(event){
@@ -104,6 +111,7 @@ export class CheckoutComponent implements OnInit {
     }
     else{
       this.checkoutFormGroup.controls.billingAddress.reset();
+      this.billingAddressStates=[];
     }
 
   }
