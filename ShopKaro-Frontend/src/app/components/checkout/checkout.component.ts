f00a51ed257cc80984f27e51ebc555016d307096
razaaasif <aasifraza9123@gmail.com>
2021-11-05 12:03:13 +0530

    added error handling in view for billing nad shipping address

--- ShopKaro-Frontend/src/app/components/checkout/checkout.component.ts
+++ ShopKaro-Frontend/src/app/components/checkout/checkout.component.ts
@@ -31,6 +31,7 @@ export class CheckoutComponent implements OnInit {
 
   ngOnInit(): void {
     this.checkoutFormGroup = this.formBuilder.group({
+
       customer:this.formBuilder.group({
         firstName:new FormControl('',[Validators.required,
                                       Validators.minLength(2),
@@ -43,27 +44,62 @@ export class CheckoutComponent implements OnInit {
         email:new FormControl('',[Validators.required, 
                                   Validators.pattern('^[a-z0-9._%+-]+@[a-z0-9.-]+\\.[a-z]{2,4}$')])
       }),
+
+
       shippingAddress:this.formBuilder.group({
-        street:[''],
-        city:[''],
-        state:[''],
-        country:[''],
-        zipCode:['']
+        street:new FormControl('',[Validators.required,
+                                  Validators.minLength(2),
+                                  ShopkaroValidators.notOnlyWhitespace]),
+
+        city:new FormControl('',[Validators.required,
+                                Validators.minLength(2),
+                                ShopkaroValidators.notOnlyWhitespace]),
+
+        state:new FormControl('',[Validators.required]),
+
+        country:new FormControl('',[Validators.required]),
+
+        zipCode:new FormControl('',[Validators.required,
+                                    Validators.minLength(2),
+                                    ShopkaroValidators.notOnlyWhitespace]),
       }),
+
+
+
       billingAddress:this.formBuilder.group({
-        street:[''],
-        city:[''],
-        state:[''],
-        country:[''],
-        zipCode:[''],
-      }),
+        street:new FormControl('',[Validators.required,
+                                    Validators.minLength(2),
+                                    ShopkaroValidators.notOnlyWhitespace]),
+
+        city:new FormControl('',[Validators.required,
+                                Validators.minLength(2),
+                                ShopkaroValidators.notOnlyWhitespace]),
+
+        state:new FormControl('',[Validators.required]),
+
+        country:new FormControl('',[Validators.required]),
+
+        zipCode:new FormControl('',[Validators.required,
+                            Validators.minLength(2),
+                            ShopkaroValidators.notOnlyWhitespace]),
+        }),
+
       creditCard :this.formBuilder.group({
-        cardType:[''],
-        nameOnCard:[''],
-        cardNumber:[''],
-        securityCode:[''],
-        expirationMonth:[''],
-        expirationYear:['']
+        cardType:new FormControl('',[Validators.required]),
+
+        nameOnCard:new FormControl('',[Validators.required,
+                                      Validators.minLength(2),
+                                      ShopkaroValidators.notOnlyWhitespace]),
+        cardNumber:new FormControl('',[Validators.required,
+                                      Validators.minLength(16),
+                                      ShopkaroValidators.notOnlyWhitespace]),
+        securityCode:new FormControl('',[Validators.required,
+                                        Validators.minLength(3),
+                                        ShopkaroValidators.notOnlyWhitespace]),
+
+        expirationMonth:new FormControl('',[Validators.required]),
+
+        expirationYear:new FormControl('',[Validators.required])
       })
     });
 
@@ -189,5 +225,26 @@ export class CheckoutComponent implements OnInit {
   get firstName(){return this.checkoutFormGroup.get('customer.firstName');}
   get lastName(){return this.checkoutFormGroup.get('customer.lastName');}
   get email(){return this.checkoutFormGroup.get('customer.email');}
+
+  get shippingAddressStreet(){return this.checkoutFormGroup.get('shippingAddress.street');}
+  get shippingAddressCity(){return this.checkoutFormGroup.get('shippingAddress.city');}
+  get shippingAddressState(){return this.checkoutFormGroup.get('shippingAddress.state');}
+  get shippingAddressCountry(){return this.checkoutFormGroup.get('shippingAddress.country');}
+  get shippingAddressZipCode(){return this.checkoutFormGroup.get('shippingAddress.zipCode');}
+
+  get billingAddressStreet(){return this.checkoutFormGroup.get('billingAddress.street');}
+  get billingAddressCity(){return this.checkoutFormGroup.get('billingAddress.city');}
+  get billingAddressState(){return this.checkoutFormGroup.get('billingAddress.state');}
+  get billingAddressCountry(){return this.checkoutFormGroup.get('billingAddress.country');}
+  get billingAddressZipCode(){return this.checkoutFormGroup.get('billingAddress.zipCode');}
+
+ //cardType nameOnCard cardNumber securityCode expirationMonth expirationYear
+  get creditCardCardType(){ return this.checkoutFormGroup.get('creditCard.cardType');}
+  get creditCardNameOnCard(){ return this.checkoutFormGroup.get('creditCard.nameOnCard');}
+  get creditCardCardNumber(){ return this.checkoutFormGroup.get('creditCard.cardNumber');}
+  get creditCardSecurityCode(){ return this.checkoutFormGroup.get('creditCard.securityCode');}
+  get creditCardExpirationMonth(){ return this.checkoutFormGroup.get('creditCard.expirationMonth');}
+  get creditCardExpirationYear(){ return this.checkoutFormGroup.get('creditCard.expirationYear');}
+
 
 }
