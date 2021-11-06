d0b2cb844dc9c940b9ee23f0333c4763964bed58
razaaasif <aasifraza9123@gmail.com>
2021-11-06 22:00:54 +0530

    secured backend

---
+++ ShopKaro-Backend/src/main/java/com/aasif/shopkaro/ShopKaroBackend/config/SecurityConfiguration.java
@@ -0,0 +1,24 @@
+package com.aasif.shopkaro.ShopKaroBackend.config;
+
+import com.okta.spring.boot.oauth.Okta;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+@Configuration
+public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        //protect endpoint  /api/orders
+        http.authorizeRequests()
+                .antMatchers("/api/orders/**")
+                .authenticated()
+                .and()
+                .oauth2ResourceServer()
+                .jwt();
+        http.cors();
+
+        //force non-empty response body for 401's to make the response more friendly
+        Okta.configureResourceServer401ResponseBody(http);
+    }
+}
