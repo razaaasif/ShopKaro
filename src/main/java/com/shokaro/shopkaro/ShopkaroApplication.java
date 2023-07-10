ea2ad4bfd69de08290b562925a23dfb56a9bb0a0
razaasif <aasifraza9123@gmail.com>
2023-07-10 19:09:29 +0530

    created new backend from scratch

---
+++ src/main/java/com/shokaro/shopkaro/ShopkaroApplication.java
@@ -0,0 +1,13 @@
+package com.shokaro.shopkaro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShopkaroApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShopkaroApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/shokaro/shopkaro/ShopkaroApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shokaro.shopkaro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShopkaroApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
